9d90713c2357ea08a5c9550709549fed13445d96
AdithyanSPillaiOfficial <adithyanspillaiofficial@gmail.com>
2024-09-19 16:09:18 +0530

    Added logout feature

--- src/Widgets/Header/Header.jsx
+++ src/Widgets/Header/Header.jsx
@@ -34,12 +34,20 @@ function Header() {
         }
     }
 
+    function handleLogout() {
+        setUname("");
+        Cookies.remove('uname');
+        setNameChange(true);
+        console.log(Cookies.get('uname'));
+    }
+
     return (
         <div className='titlebar'>
             <div onClick={() => router.replace("/")}><span>U Type</span></div>
             <div className='nameinput'>
                 {nameChange ? <input type="text" placeholder='Your Name' value={uname != "null" ? uname : ""} onChange={(e) => setUname(e.target.value)} /> : <span>{Cookies.get('uname')}</span>}
                 <div className='button' onClick={handleBtnClick}>{nameChange ? 'Apply' : 'Edit'}</div>
+                {uname != "" ? <div className='button' onClick={handleLogout}>Logout</div> : null}
             </div>
             <div className='endpanel'>
                 <div className='button' onClick={() => setTogglePopup(true)}>Set Custom Text</div>
